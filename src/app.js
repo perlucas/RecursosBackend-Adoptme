43c78067996a807fa45630803a387174567c5deb
Lucas Pereyra <lukaspereyra8@gmail.com>
2024-07-20 12:08:25 -0300

    fixed views path

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.set('views', 'src/views');
 
 
 app.use('/', viewsRouter)
